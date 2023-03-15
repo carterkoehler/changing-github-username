179a580b1b9370b25e8ccd50232cdbefd840590b
carterkoehler <carterleekoehler@gmail.com>
2023-03-14 21:22:55 -0400

    Fixed error

--- script-writer.sh
+++ script-writer.sh
@@ -1,4 +1,4 @@
 touch replace-commands.sh
 find ~ | grep "/.git/config" > replace-commands.sh
 sed -i "s/^/sed -i \"s\/$1\/$2\/\" /g" update-configs.sh
-chmod +x replace-commands.sh
+chmod +x update-configs.sh
